fea97eb2bc084d0d155b17923a11e2b3593d6e55
vdnh <vdnh@yahoo.com>
2017-12-29 12:33:55 -0500

    the main change.

--- src/githubtest/GithubTest.java
+++ src/githubtest/GithubTest.java
@@ -15,8 +15,11 @@ public class GithubTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("Hello two !");
+
         System.out.println("Hello World !");
+
     }
     
 }
